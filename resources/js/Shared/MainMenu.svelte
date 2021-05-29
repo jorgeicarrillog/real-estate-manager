65c67ce0382bddf8cdbd7288d9b886fe70a90568
Jorge Carrillo <jorgei.carrillog@autonoma.edu.co>
2021-05-28 19:32:14 -0500

    Initial commit project

---
+++ resources/js/Shared/MainMenu.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let className = '';
+
+    import MainMenuItem from '@/Shared/MainMenuItem.svelte';
+</script>
+
+<div class={className}>
+    <MainMenuItem text="Dashboard" link="dashboard" icon="dashboard"/>
+    <MainMenuItem text="Inmobiliarias" link="organizations" icon="office"/>
+    <MainMenuItem text="Usuarios" link="users" icon="users"/>
+    <MainMenuItem text="Reports" link="reports" icon="printer"/>
+</div>
